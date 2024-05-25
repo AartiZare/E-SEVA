855a59a4569f8ed89da780fde2fabd91ed9759aa
Aarti Zare <aarti.z@pacewisdom.com>
2024-05-25 12:48:16 +0530

    migration changes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,14 +1,27 @@
+// export default {
+//     HOST: 'localhost',
+//     USER: 'postgres',
+//     PASSWORD: 'Pacewisdom@12345',
+//     DB: 'New_Project',
+//     dialect: 'postgres',
+//     logging: true,
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// }
+
 export default {
-    HOST: 'localhost',
-    USER: 'postgres',
-    PASSWORD: 'Pacewisdom@12345',
-    DB: 'New_Project',
+    HOST: '52.66.238.70',
+    // port: 5432,
+    // logging: console.log,
+    maxConcurrentQueries: 100,
     dialect: 'postgres',
-    logging: true,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+  USER: 'postgres',
+    PASSWORD: 'Welcome123#',
+    DB: 'e-sevadev',
+    pool: { maxConnections: 5, maxIdleTime: 30},
+    language: 'en'
 }
