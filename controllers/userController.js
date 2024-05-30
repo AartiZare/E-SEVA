a704854723b122d3b3c455bd51ebe06cb7ec3a2c
ya55sh <yashbahuguna10@gmail.com>
2024-05-30 15:13:20 +0530

    -remove duplicate code

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,11 +21,10 @@ const userBranchModel = db.UserBranch;
 const vendorModel = db.Vendor;
 
 export const create = catchAsync(async (req, res, next) => {
-
     try {
       const { body } = req;
       const branchId = body.branch[0];
-
+  
       body.email_id = body.email_id.toLowerCase();
   
       const user = await userModel.findOne({
@@ -40,44 +39,16 @@ export const create = catchAsync(async (req, res, next) => {
       if (user) {
         if (user.email_id === body.email_id && user.contact_no !== body.contact_no) {
           return next(new ApiError(httpStatus.BAD_REQUEST, `Email ${body.email_id} is already in use!`));
-        
-        body.email_id = body.email_id.toLowerCase();
-
-        const user = await userModel.findOne({
-            where: {
-                [Op.or]: [
-                    { email_id: body.email_id },
-                    { contact_no: body.contact_no }
-                ]
-            }
-        });
-
-        if (user) {
-            if (user.email_id === body.email_id && user.contact_no !== body.contact_no) {
-                return next(new ApiError(httpStatus.BAD_REQUEST, `Email ${body.email_id} is already in use!`));
-            }
-            if (user.contact_no === body.contact_no && user.email_id !== body.email_id) {
-                return next(new ApiError(httpStatus.BAD_REQUEST, `Phone number ${body.contact_no} is already in use!`));
-            }
-            if (user.email_id === body.email_id && user.contact_no === body.contact_no) {
-                return next(new ApiError(httpStatus.BAD_REQUEST, 'User already exists'));
-            }
         }
-
-        const resetPasswordToken = jwt.sign({ email_id: body.email_id }, secretKey, { expiresIn: '6h' });
-
-        let profileImageUrl;
-        if (req.file) {
-            profileImageUrl = `http://52.66.238.70/E-Seva/uploads/${req.file.originalname}`;
+        if (user.contact_no === body.contact_no && user.email_id !== body.email_id) {
+          return next(new ApiError(httpStatus.BAD_REQUEST, `Phone number ${body.contact_no} is already in use!`));
         }
-
-        const userData = { ...body, resetPasswordToken };
-        if (profileImageUrl) {
-            userData.profile_image = profileImageUrl;
+        if (user.email_id === body.email_id && user.contact_no === body.contact_no) {
+          return next(new ApiError(httpStatus.BAD_REQUEST, 'User already exists'));
         }
       }
   
-      const resetPasswordToken = genToken({ email: body.email_id });
+      const resetPasswordToken = jwt.sign({ email_id: body.email_id }, secretKey, { expiresIn: '6h' });
   
       let profileImageUrl;
       if (req.file) {
@@ -90,15 +61,15 @@ export const create = catchAsync(async (req, res, next) => {
       }
   
       const createdUser = await userService.createUser(userData);
-
+  
       // Verify if the branch exists
       const branch = await branchModel.findByPk(parseInt(branchId));
       if (!branch) {
         return next(new ApiError(httpStatus.BAD_REQUEST, 'Branch not found'));
       }
   
-    // Update the userBranchModel with the new branch
-    const existingUserBranch = await userBranchModel.findOne({
+      // Update the userBranchModel with the new branch
+      const existingUserBranch = await userBranchModel.findOne({
         where: {
           userId: createdUser.id,
           branchId: branchId,
@@ -112,7 +83,7 @@ export const create = catchAsync(async (req, res, next) => {
           status: true
         });
       }
-
+  
       const emailSubject = "Set Your Password";
       const emailText = `To set your password, use the following URL: http://localhost:3000/set-password?token=${resetPasswordToken}`;
       const emailHtml = `<p>To set your password, click <a href="http://localhost:3000/set-password?token=${resetPasswordToken}">here</a>.</p>`;
@@ -124,10 +95,10 @@ export const create = catchAsync(async (req, res, next) => {
         results: createdUser,
       });
     } catch (error) {
-        console.error(error);
-        return res.status(500).send({ error: 'Internal Server Error' });
+      console.error(error);
+      return res.status(500).send({ error: 'Internal Server Error' });
     }
-});  
+  });  
 
 export const set_password = catchAsync(async (req, res, next) => {
     try {
