ce9ce988178395deea943da5f9a839641a4b0fee
Sanjeev Shetty <sanjeevkse@gmail.com>
2024-06-10 13:39:36 +0530

    feat: profile image update

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -5,32 +5,54 @@ import fs from "fs";
 // Set up storage for uploaded files
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const branchName = req.body.branch_name;
-    // const uploadPath = path.join(__dirname, 'uploads', branchName);
-    const uploadPath = `public/uploads/${branchName}`;
+    if (req.body.branch_name) {
+      const branchName = req.body.branch_name;
+      // const uploadPath = path.join(__dirname, 'uploads', branchName);
+      const uploadPath = `public/uploads/${branchName}`;
+  
+      // Check if the directory exists
+      if (!fs.existsSync(uploadPath)) {
+          // Create the directory if it does not exist
+          fs.mkdirSync(uploadPath, { recursive: true }, (err) => {
+            if (err) {
+                console.error('Error creating directory:', err);
+                cb(err, uploadPath);
+            }
+        });
+      }
+  
+      cb(null, uploadPath);
+    } else {
+      const uploadPath = `public/uploads/profileImages`;
 
-    // Check if the directory exists
-    if (!fs.existsSync(uploadPath)) {
-        // Create the directory if it does not exist
-        fs.mkdirSync(uploadPath, { recursive: true }, (err) => {
-          if (err) {
-              console.error('Error creating directory:', err);
-              cb(err, uploadPath);
-          }
-      });
+      // Check if the directory exists
+      if (!fs.existsSync(uploadPath)) {
+          // Create the directory if it does not exist
+          fs.mkdirSync(uploadPath, { recursive: true }, (err) => {
+            if (err) {
+                console.error('Error creating directory:', err);
+                cb(err, uploadPath);
+            }
+        });
+      }
+
+      cb(null, uploadPath);
     }
-
-    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
-    const newFilename = `${req.body.document_reg_no}${path.extname(file.originalname)}`;
-    cb(null, newFilename);
+    if (req.body.document_reg_no) {
+      const newFilename = `${req.body.document_reg_no}${path.extname(file.originalname)}`;
+      cb(null, newFilename);
+    } else {
+      const newFilename = `${req.body.contact_no}${path.extname(file.originalname)}`;
+      cb(null, newFilename);
+    }
   },
 });
 // Create the multer instance
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 1024 * 1024 * 5 },
+  limits: { fileSize: 1024 * 1024 * 200 },
 });
 
 export default upload;
